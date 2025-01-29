0cb30d1fe6e3d65adf92d21141e5899bc0ed4f80
Mario Cabrera <yankeescab@gmail.com>
2025-01-28 20:33:57 -0500

    Began adding basic API's to call the categories and products

---
+++ app/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request
+from data.bakery_db import categories, products
+
+app = Flask(__name__)
+
+
+## GET Requests
+
+
+# Category
+
+
+@app.get("/category")
+def get_categories():
+    """
+    Retrieves all categories from the bakery.
+
+    Returns:
+        categories: The specified category.
+
+    Response Codes:
+        200: Successful Request.
+    """
+    return {"categories": categories}, 200
+
+
+@app.get("/category/{category_id}")
+def get_category_by_id(category_id):
+    """
+    Retrieves a specific category by its ID.
+
+    Parameters:
+    category_id (integer): category identifier.
+
+    Returns:
+        category: The specified category.
+
+    Response Codes:
+        200: Successful Request.
+        400: The category ID given does not exist/did not fetch anything.
+    """
+    try:
+        return categories[category_id], 200
+    except KeyError:
+        return 404
+
+
+# Product
+
+
+@app.get("/product")
+def get_products():
+    """
+    Retrieves all products from the bakery.
+
+    Returns:
+        products: The specified category.
+
+    Response Codes:
+        200: Successful Request.
+    """
+    return {"products": products}, 200
+
+
+## POST Requests
+
+# Category
+
+
+@app.post("/category")
+def create_category():
+    try:
+        request_data = request.get_json()
+
+        new_category = {
+            "category_id": (max(categories.keys()) + 1),
+            "category_name": request_data["category_name"],
+        }
+        categories.append(new_category)
+        return new_category, 201
+
+    except KeyError:
+        return 400
---
+++ app/data/bakery_db.py
@@ -0,0 +1,87 @@
+categories = {
+    1: {
+        "category_id": 1,
+        "category_name": "Breads"
+    },
+    2: {
+        "category_id": 2,
+        "category_name": "Rolls"
+    },
+    3: {
+        "category_id": 3,
+        "category_name": "Pastries"
+    }
+}
+
+products = {
+    1: {
+        "product_id": 1,
+        "category_id": 1,
+        "product_code": "ryeB",
+        "product_name": "Rustic Rye Bread",
+        "price": 3.49
+    },
+    2: {
+        "product_id": 2,
+        "category_id": 1,
+        "product_code": "wwB",
+        "product_name": "Whole Wheat Bread",
+        "price": 2.99
+    },
+    3: {
+        "product_id": 3,
+        "category_id": 1,
+        "product_code": "srdB",
+        "product_name": "Sourdough Bread",
+        "price": 3.99
+    },
+    4: {
+        "product_id": 4,
+        "category_id": 1,
+        "product_code": "mltB",
+        "product_name": "Multigrain Bread",
+        "price": 4.49
+    },
+    5: {
+        "product_id": 5,
+        "category_id": 1,
+        "product_code": "sndB",
+        "product_name": "Sandwich Bread",
+        "price": 1.99
+    },
+    6: {
+        "product_id": 6,
+        "category_id": 1,
+        "product_code": "brgB",
+        "product_name": "Barguette",
+        "price": 2.99
+    },
+    7: {
+        "product_id": 7,
+        "category_id": 2,
+        "product_code": "plnR",
+        "product_name": "Plain Wheat Roll",
+        "price": 0.79
+    },
+    8: {
+        "product_id": 8,
+        "category_id": 2,
+        "product_code": "ssmR",
+        "product_name": "Sesame Roll",
+        "price": 0.99
+    },
+    9: {
+        "product_id": 9,
+        "category_id": 3,
+        "product_code": "bclP",
+        "product_name": "Bear Claw",
+        "price": 1.99
+    },
+    10: {
+        "product_id": 10,
+        "category_id": 3,
+        "product_code": "chrP",
+        "product_name": "Cherry Pie",
+        "price": 4.89
+    }
+}
